95e6185f669a9cda00e4ec336c1a957d5b917106
mattcusprime <englmt1@gmail.com>
2016-05-06 17:34:23 -0400

    Added own js file and think got the map transparency issue fixed.

---
+++ js/myFile.js
@@ -0,0 +1,32 @@
+﻿var funDatCbackTho = function () { alert('I GOT CALLED BACK YO') };
+var urlGoogMapsUrl = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCzyiqBO_5XdhIyozK30VcFyDKZiUD_2nw';
+function calculateRoute() {
+    var table = $('#AddressesWithItems').DataTable();
+    var data = table.rows().data();
+    //console.log(data);
+    for (var i = 0; i < data.length; i++) {
+        var objCurrentRow = data[i];
+        var strCurrentAddress = objCurrentRow.street + "," + objCurrentRow.city + "," + objCurrentRow.state + "," + objCurrentRow.zip_code;
+        strCurrentAddress = strCurrentAddress.replace(/\s/g, '+');
+        console.log(strCurrentAddress);
+
+    };
+
+};
+
+
+
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8
+    });
+}
+
+function getUrlAndCallback(url, callBack) {
+    //get script working much better than just standard $.get
+    $.getScript(url, function (data) { }).done(callBack);
+
+};
+
